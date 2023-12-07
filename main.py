f83c4e725571078c2569c81b04a05f38634e6082
gba-exe <gabriel.baraujo@sptech.school>
2023-12-07 13:10:45 -0300

    feat(): comentando codigo

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+# importando um dataset de numeros
 mnist = tf.keras.datasets.mnist
+
+# criando uma matriz com treinamentos e testes (treinamento = para treinar) (testes = ver se deu certo)
 (train_images, train_labels) , (test_images, test_labels) = mnist.load_data()
 
+# mostrando o dataset criado
 print("train_images shape: ", train_images.shape)
 print("train_labels shape: ", train_labels.shape)
 print("test_images shape: ", test_images.shape)
 print("test_labels shape: ", test_labels.shape)
 
 
+# plotando o dataset
 fig = plt.figure(figsize=(10,10))
 
 nrows=3
